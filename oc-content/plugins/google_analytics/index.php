139ad6f776dccf0a421f5c34099c8ba8df955abe
Frankie Fong <frankiefonghk@yahoo.com>
2016-08-18 16:58:48 +0800

    update to use Universal Analytics method
    old 'ga()' is deprecated

--- oc-content/plugins/google_analytics/index.php
+++ oc-content/plugins/google_analytics/index.php
@@ -61,6 +61,13 @@ Plugin update URI: http://www.osclass.org/files/plugins/google_analytics/update.
             require_once(osc_plugins_path() . 'google_analytics/footer.php');
         }
     }
+    
+    function google_analytics_header() {
+        if( osc_google_analytics_id() != '' ) {
+            $id = osc_google_analytics_id();
+            require_once(osc_plugins_path() . 'google_analytics/analyticstracking.php');
+        }
+    }
 
     function google_admin_menu() {
         osc_admin_menu_plugins('Google Analytics', osc_admin_render_plugin_url('google_analytics/admin.php'), 'google_analytics_submenu');
@@ -71,7 +78,7 @@ Plugin update URI: http://www.osclass.org/files/plugins/google_analytics/update.
     // This is a hack to show a Uninstall link at plugins table (you could also use some other hook to show a custom option panel)
     osc_add_hook(osc_plugin_path(__FILE__)."_uninstall", 'google_analytics_call_after_uninstall');
     osc_add_hook(osc_plugin_path(__FILE__)."_configure", 'google_analytics_admin');
-    osc_add_hook('footer', 'google_analytics_footer');
+    osc_add_hook('before-header', 'google_analytics_header');
     osc_add_hook('admin_menu_init', 'google_admin_menu');
 
 ?>
